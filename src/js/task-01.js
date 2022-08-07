5b82b40c66b4666336cae4770ba6b43697456791
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-08-07 20:25:56 +0300

    Добавляет готовое Task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+// Напиши скрипт который:
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>)
+// и количество элементов в категории(всех вложенных в него < li >).
+
+const navItemEl = document.querySelectorAll(".item");
+console.log("Number of categories: ", navItemEl.length);
+
+navItemEl.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent);
+  console.log("Elements: ", item.lastElementChild.children.length);
+});
