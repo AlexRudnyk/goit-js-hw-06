253063d3b31949775aca759095bb591c215bb763
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-08-10 20:19:11 +0300

    Исправляет Task-06

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,12 @@
 const inputEl = document.querySelector("#validation-input");
 console.log("inputEl", inputEl);
 
-inputEl.addEventListener("blur", () => {
-  if (inputEl.value.length == inputEl.dataset.length) {
+inputEl.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length == inputEl.dataset.length) {
     inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
   } else {
     inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 });
